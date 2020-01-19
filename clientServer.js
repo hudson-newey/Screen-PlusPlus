fcfdae276acd8421aca584a546d8a7d96e8772bb
Grathium Industries <GrathiumIndustries@gmail.com>
2020-01-19 11:41:24 +1000

    bug fixes

--- clientServer.js
+++ clientServer.js
@@ -1,11 +1,16 @@
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var q = url.parse(req.url, true).query;
-  var websiteUSR = q.password;
-  res.end(websiteUSR);
-  const {exec} = require("child_process")
-  exec(websiteUSR).unref()
-}).listen(4422);
+  fs.readFile('./index.html', function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    
+    return res.end("");
+  });
+}).listen(8080);
