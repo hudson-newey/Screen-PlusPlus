1d5f5cfed6e2d242bbc304306c4ef1269cf26895
Grathium Industries <GrathiumIndustries@gmail.com>
2020-01-21 12:16:39 +1000

    improved user experiance

--- screen.js
+++ screen.js
@@ -4,7 +4,8 @@ var url = require('url');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   var websiteUSR = (url.parse(req.url, true).query).v;
-  res.end("\"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe\" " + websiteUSR);
+  websiteUSR = "\"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe\" " + websiteUSR;
+  res.end(websiteUSR);
 
   var {exec} = require("child_process");
   exec(websiteUSR).unref();
