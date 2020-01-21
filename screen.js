07f04d668e0f061cb79720b6d63866649aef9f3e
Grathium Industries <GrathiumIndustries@gmail.com>
2020-01-21 11:48:17 +1000

    finished version 1

--- screen.js
+++ screen.js
@@ -1,20 +1,11 @@
 var http = require('http');
 var url = require('url');
 
-// readline function
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var websiteUSR = (url.parse(req.url, true).query).v;
+  res.end("\"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe\" " + websiteUSR);
 
-// create password
-readline.question(`Create Password: `, (password) => {
-  http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var q = url.parse(req.url, true).query;
-    var websiteUSR = q.password;
-    res.end(websiteUSR);
-    const {exec} = require("child_process")
-    exec(websiteUSR).unref()
-  }).listen(8080);
-})
+  var {exec} = require("child_process");
+  exec(websiteUSR).unref();
+}).listen(8080);
