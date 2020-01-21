84ad5f492a42a9ea4cee60472ea2a591569d4b91
Grathium Industries <GrathiumIndustries@gmail.com>
2020-01-21 17:30:57 +1000

    added commands starting with an exclemation point

--- screen.js
+++ screen.js
@@ -3,8 +3,19 @@ var url = require('url');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  var websiteUSR = (url.parse(req.url, true).query).v;
-  websiteUSR = "\"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe\" " + websiteUSR;
+  var websiteUSR = (url.parse(req.url, true).query).w;
+
+  if (!(websiteUSR.includes("!"))) {
+  	websiteUSR = "\"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe\" " + websiteUSR;
+  } else {
+  	switch(websiteUSR) {
+  		case "!close":
+    		websiteUSR = "\"./mod/close.bat\"";
+    		break;
+  		default:
+    		websiteUSR = "\"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe\" " + websiteUSR;
+	}
+  }
   res.end(websiteUSR);
 
   var {exec} = require("child_process");
